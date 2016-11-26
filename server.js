0cc2b2a0cb3e2c9d6a181be3f169115d25e6ce97
hobbyling <g99949@hotmail.com>
2016-11-26 12:25:56 +0800

    edit ejs

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ var app = express();
  //index page
  app.get('/', function(req, res) {  
  	var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
+        { name: 'Hobby Lee', number: 'M1043303' },
+        { name: 'JingTing', number: 'A10xxxxx' },
     ];
     var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
