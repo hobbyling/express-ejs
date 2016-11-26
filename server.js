b26c900aa5f46bed72818ff4a1a21a3a4d606144
hobbyling <g99949@hotmail.com>
2016-11-26 22:58:33 +0800

    edit server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 
 //set the view engine to ejs 
 //how we send a view to the user by using res.render(), res.render() will look in a views folder for the view, so we only have to define "page/index"
- app.set('view engine', 'ejs');  
+app.set('view engine', 'ejs');  
+app.set('port', (process.env.PORT || 3000));
 
  //use res.render渲染 to load up an ejs view file 
 
@@ -12,7 +13,7 @@ var app = express();
  app.get('/', function(req, res) {  
  	var drinks = [
         { name: 'Hobby Lee', number: 'M1043303' },
-        { name: 'JingTing', number: 'A10xxxxx' },
+        { name: 'JingTing', number: 'A1023306' },
     ];
     var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
@@ -27,6 +28,6 @@ var app = express();
  	res.render('pages/about');
  });  
 
- var server = app.listen(3000, function() {  
+var server = app.listen(app.get('port'), function(){  
   console.log('Listening on port 3000');  
  });
